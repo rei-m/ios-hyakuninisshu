af02d542a40836eb19d1b263914765bd52d3db07
rei-m <star.oceans.rm@gmail.com>
2020-11-27 06:30:43 +0900

    Initial Commit

---
+++ Hyakuninisshu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hyakuninisshu
+//
+//  Created by Rei Matsushita on 2020/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
