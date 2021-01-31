00a5813e5dbcd9748666b94c1ab7210c7a930c3b
rei-m <star.oceans.rm@gmail.com>
2021-02-01 00:08:30 +0900

    add PrivacyPolicy

---
+++ Hyakuninisshu/Presentations/Features/Setting/PrivacyPolicyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PrivacyPolicyViewController.swift
+//  Hyakuninisshu
+//
+//  Created by Rei Matsushita on 2021/02/01.
+//
+
+import UIKit
+
+class PrivacyPolicyViewController: UIViewController {
+    @IBAction func didTapGooglePrivacyPolicy(_ sender: Any) {
+        guard let url = URL(string: "https://policies.google.com/privacy?hl=ja") else {
+            fatalError()
+        }
+
+        if (UIApplication.shared.canOpenURL(url)) {
+            UIApplication.shared.open(url)
+        }
+    }
+}
