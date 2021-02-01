109fc21c137878320a9382f31660c07926129dbf
rei-m <star.oceans.rm@gmail.com>
2021-02-01 09:41:26 +0900

    add version

--- Hyakuninisshu/Presentations/Features/Setting/SettingViewController.swift
+++ Hyakuninisshu/Presentations/Features/Setting/SettingViewController.swift
@@ -9,15 +9,11 @@ import UIKit
 
 class SettingViewController: UITableViewController {
     @IBOutlet weak var darkModeValueLabel: UILabel!
+    @IBOutlet weak var appVersionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        appVersionLabel.text = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
     }
 
     override func viewWillAppear(_ animated: Bool) {
